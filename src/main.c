249e06f8764ded16137ed66ddee5708782907fad
Rustam <rustam@debian>
2026-08-04 12:01:33 +0400

    Add basic layout

--- src/main.c
+++ src/main.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ncurses.h>
 
 int main() {
-    initscr();
+    char msg1[] = "username: ";
+    char user[64];
+    int userX, userY;
+    char msg2[] = "password: ";
+    char pass[64];
+    int passX, passY;
+    int rows, cols;
 
-    printw("ncdm");
+    initscr();
+    getmaxyx(stdscr, rows, cols);
+
+    mvprintw(rows/2-1, (cols-10-strlen(msg1))/2, "%s", msg1);
+    getyx(stdscr, userY, userX);
+    mvprintw(rows/2+1, (cols-10-strlen(msg2))/2, "%s", msg2);
+    getyx(stdscr, passY, passX);
+
+    move(userY, userX);
+    getstr(user);
+    move(passY, passX);
+    getstr(pass);
 
     refresh();
-    getch();
+
     endwin();
 
     return 0;
